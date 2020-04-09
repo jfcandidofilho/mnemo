40ba329009d33f9c27a6b51942e024265a1a33f9
J. F. Candido Filho <jfcandidofilho@gmail.com>
2020-04-08 23:55:41 -0300

    MINOR Add elementary page with title and canvas
    This is just a simple commit that has a simple index.php page with the
    game title and a canvas set up. It also has two external file linked
    (both are JavaScript and CSS files).
    
    The page exhibits the title "MNEMO" and a canvas with border (debug).
    The page also outputs in console if the JavaScript code was executed.

---
+++ canvas.js
@@ -0,0 +1,3 @@
+
+// DEBUG - To be left at the end of the code
+console.log( "SCRIPT FULLY EXECUTED" );
---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+
+    <head>
+    
+        <title>MNEMO</title>
+    
+        <link rel="stylesheet" type="text/css" href="./layout.css" />
+    
+        <script type="text/javascript" src="./canvas.js"></script>
+    
+    </head>
+    
+    <body>
+    
+        <div id="title">
+            MNEMO
+        </div>
+    
+        <canvas id="mnemo" width="600px" height="600px"></canvas>
+    
+    </body>
+
+</html>
