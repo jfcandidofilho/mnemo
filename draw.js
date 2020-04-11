50e9d9c3cc5f5e237980898e2e3bec5bf97550cb
J. F. Candido Filho <jfcandidofilho@gmail.com>
2020-04-11 05:07:12 -0300

    MINOR Add support for flipping a pair of cards
    This adds support for flipping cards. The number of cards able to be
    flipped at a given time is two. Note that "flipping" means to make the
    clicked card either face up or down - depends on its previous state.
    
    This commit also includes a randomizer of cards based on the
    Fisher-Yates algorithm. To be specific, it uses the revised version by
    Durstenfeld that applies it to computers.
    
    The code also:
    
    - set/get the state of the card (face down/face up);
    - set/get active cards (cards faced up);
    - get paired cards (not usable because there is no set function);
    - avoids copy by reference with non-primitive types (throw/catch);
    - forces copy by reference with var declaration to search non-primitives
      inside arrays with indexOf;
    - sets click event for every card;

--- draw.js
+++ draw.js
@@ -65,6 +65,28 @@ function image_directory( type = "arabic", position = 0, isFG ){
     
 }
 
+// Random table with Fisher-Yates algorithm modernized by Durstenfeld for computers
+function fisher_yates( sorted_array ) {
+
+    // Source: https://stackoverflow.com/a/2450976/1494516
+    // Source: https://stackoverflow.com/a/12646864/1494516 (probably this one)
+
+    var i, random, temp;
+
+    for( i = sorted_array.length - 1; i > 0; i-- ) {
+
+        random = Math.floor( Math.random() * ( i + 1 ) );
+
+        temp                    = sorted_array[ i ];
+        sorted_array[ i ]       = sorted_array[ random ];
+        sorted_array[ random ]  = temp;
+
+    }
+
+    return sorted_array;
+
+}
+
 // Set a card into destination
 function set_card( row, cell, type = "arabic", position = 0, isFG ){
 
@@ -108,6 +130,75 @@ function get_cards_row( row, cell ){
 }
 
 // 
+function set_backforeground( cards, position ){
+
+    cards[ position ][2] = ! cards[ position ][2];
+
+}
+
+//
+function get_backforeground( cards, position ){
+
+    return cards[ position ][2];
+
+}
+
+//
+function get_active_cards(){
+
+    return( cards.active );
+
+}
+
+//
+function set_active_cards( value, index_position = 0, html_position = [] ){
+
+    cards.active.number += value;
+
+    if( value > 0 ) { 
+        
+        cards.active.index_positions.push( index_position ); 
+
+        cards.active.html_positions.push( html_position );
+    
+    } else if( value < 0 ){ 
+        
+        console.log( 
+            "UNSET active : index_positions", 
+            cards.active.index_positions.splice( 
+                cards.active.index_positions.indexOf( index_position ),
+                1
+            ) 
+        );
+
+        console.log( 
+            "UNSET active : html_positions", 
+            cards.active.html_positions.splice( 
+                cards.active.html_positions.indexOf( html_position ),
+                1
+            ) 
+        );
+
+    } else ;
+
+    console.log( "VAR cards.active: val i l (index)" );
+    console.log( value, index_position, cards.active.index_positions );
+
+    console.log( "VAR cards.active: val i l (html)" );
+    console.log( value, html_position, cards.active.html_positions );
+
+}
+
+//
+function get_paired_cards(){
+
+    console.log( "fn get_paired..", cards.paired.html_position );
+
+    return cards.paired;
+
+}
+
+// 
 function update_score( id ){
 
     document.getElementById( id ).innerHTML = score[ id ];
@@ -144,7 +235,10 @@ function start(){
     index_position,
     
     // Card details
-    card;
+    card,
+    
+    // Every thing needed to be throwed into the onclick event function(){}
+    stuff;
 
 
     // Sets everything anew
@@ -153,6 +247,9 @@ function start(){
     cards.active.index_positions = [];
     cards.paired.index_position = [];
     cards.paired.html_position = [];
+
+    // Randomizes the locations of cards
+    cards.shuffled = fisher_yates( JSON.parse( JSON.stringify( cards.ordered ) ) );
 
     // Set the cards faced down
     set_table( false );
@@ -173,6 +270,72 @@ function start(){
 
         // Defines the position of the card in the HTML
         card = cards.shuffled[ index_position ];
+
+        // Throwable content (see next)
+        stuff = { 
+             
+            e : e, 
+            row : row, cell : cell,
+            card : card, 
+            index_position : index_position
+
+        };
+
+        // Throws content to be used inside a function(){} that would be otherwise unreachable
+        try{ throw stuff; } catch( everything ) {
+
+            // Source: https://dzone.com/articles/why-does-javascript-loop-only-use-last-value
+
+            everything.e.onclick = function() { 
+                
+                // Renaming
+                var row             = everything.row;
+                var cell            = everything.cell;
+                var index_position  = everything.index_position;
+                var card            = everything.card;
+
+                // Declares active cards variables
+                var position = [];
+                
+                // DEBUG
+                console.table( get_paired_cards() );
+                console.log( get_paired_cards().index_position );
+
+                // Stops the event if the card is already paired;
+                if( get_paired_cards().index_position.includes( index_position ) ) return ; else ;
+
+                // Declares the variable that defines which side to use
+                var side;
+
+                // Defines the HTML position ina variable to be stored with reference (for use with indexOf)
+                position = [ row, cell ];
+
+                // Turning cards up and down
+                if( get_active_cards().number < 2 ){
+
+                    // Set to turn upside
+                    side = get_backforeground( cards.shuffled, index_position );
+
+                    // Contabilizes the card
+                    set_active_cards( side ? 1 : -1, index_position, position );
+                    set_backforeground( cards.shuffled, index_position );
+
+                // Unturn if turned
+                } else if( ! get_backforeground( cards.shuffled, index_position ) ){
+
+                    side = false;
+
+                    set_active_cards( -1, index_position, position );
+                    set_backforeground( cards.shuffled, index_position );
+
+                } else ;
+
+                // Set the card based on the side
+                set_card( row, cell, card[1], card[0], side );
+
+            };
+
+        }
 
         // Disables dragging
         e.ondragstart = function(){ return false; };
@@ -197,8 +360,6 @@ window.addEventListener(
     false 
     
     );
-
-
 
 // DEBUG - To be left at the end of the code
 console.log( "SCRIPT FULLY EXECUTED" );
